bb453fe8f66ca14ae1654f09b49cbb290637d487
Jackson Tian <shyvo1987@gmail.com>
2012-11-23 16:16:16 +0800

    Add 0.0.2

--- test/index.test.js
+++ test/index.test.js
@@ -39,6 +39,7 @@ describe('iwalk', function () {
   });
   
   it('end', function (done) {
+    var walker = new Walker();
     //源路径
     var source = path.resolve(__dirname, '..');
     //测试：遍历文件夹时跳过node_modules和.git目录
@@ -68,8 +69,9 @@ describe('iwalk', function () {
   
   it('end', function (done) {
     //创建一个空目录测试是否已经解决空目录导致end事件不触发的问题
-    if (!fs.existsSync('./testEmptyDir')) {
-      fs.mkdirSync('./testEmptyDir');
+    var emptyDir = path.join(__dirname, 'testEmptyDir');
+    if (!fs.existsSync(emptyDir)) {
+      fs.mkdirSync(emptyDir);
     }
     //源路径
     var source = path.resolve(__dirname, '..');
